ac968b9de997cfea5413345688470a1f2ba7236a
Alex Panin <alexanderpanin7@gmail.com>
2025-05-08 17:40:17 +0300

    Initial commit: Add basic GuiCalculator main class

---
+++ src/main/java/com/example/todo/Calculator.java
@@ -0,0 +1,18 @@
+package com.example.todo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Простой Калькулятор");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(300, 200);
+                frame.setVisible(true);
+                System.out.println("Калькулятор запущен!");
+            }
+        });
+    }
+}
